d48783c3f7941d3afaccaaa4e81899a9d05101d6
Saljo Edassery <saljoej@gmail.com>
2020-09-07 16:47:38 +0530

    Updates

---
+++ precache-manifest.23c5278e101324fedebc63777781b46c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b200118f076fc78c12f448197199cb",
+    "url": "/sudoku-game/index.html"
+  },
+  {
+    "revision": "7ddd84ceabbf1eae1c14",
+    "url": "/sudoku-game/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "e15f62a58167dbff8037",
+    "url": "/sudoku-game/static/css/main.617ccb5d.chunk.css"
+  },
+  {
+    "revision": "7ddd84ceabbf1eae1c14",
+    "url": "/sudoku-game/static/js/2.ebffe799.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku-game/static/js/2.ebffe799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15f62a58167dbff8037",
+    "url": "/sudoku-game/static/js/main.efb23ef1.chunk.js"
+  },
+  {
+    "revision": "4be79801ef8ff108e834",
+    "url": "/sudoku-game/static/js/runtime-main.7727e577.js"
+  }
+]);
